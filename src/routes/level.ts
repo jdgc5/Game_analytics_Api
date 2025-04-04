c4d52882ca14570ed13f7cdeab051227888ceb6a
ATOJ5 <dgc105@gmail.com>
2025-04-04 17:19:55 +0200

    fixing routes level

--- src/routes/level.ts
+++ src/routes/level.ts
@@ -2,11 +2,11 @@ import express from 'express';
 import { saveLevel, getLevel, getAllLevels, updateLevel, deleteLevel } from '../controllers/levelController';
 
 const router = express.Router();
-router.post('/levels', saveLevel);
-router.get('/levels/:id', getLevel);
-router.get('/levels/user/:userId', getAllLevels);
-router.put('/levels/:id', updateLevel);
-router.delete('/levels/:id', deleteLevel);
+router.post('/', saveLevel);
+router.get('/:id', getLevel);
+router.get('/user/:userId', getAllLevels);
+router.put('/:id', updateLevel);
+router.delete('/:id', deleteLevel);
 
 export default router;
 
